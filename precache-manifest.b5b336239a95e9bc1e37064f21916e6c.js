8f6484b9d69b00dbdaefcc5b97c36b085c161056
jogspokoen88 <jogspokoen88@mail.ru>
2020-07-27 14:23:25 +0500

    Updates

---
+++ precache-manifest.b5b336239a95e9bc1e37064f21916e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c521f380fc2db3d76e4e2d7e609133f2",
+    "url": "/React-Forms/index.html"
+  },
+  {
+    "revision": "8fab4064a0e052fa16e5",
+    "url": "/React-Forms/static/css/main.c7b0f023.chunk.css"
+  },
+  {
+    "revision": "ca60a18dd369c10d67c7",
+    "url": "/React-Forms/static/js/2.6da80896.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Forms/static/js/2.6da80896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fab4064a0e052fa16e5",
+    "url": "/React-Forms/static/js/main.129edb9f.chunk.js"
+  },
+  {
+    "revision": "2279f1079373769cff6f",
+    "url": "/React-Forms/static/js/runtime-main.5344b3b2.js"
+  }
+]);
